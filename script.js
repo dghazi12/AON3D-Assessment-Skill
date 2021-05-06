0a69565a1374ba69622972a8200d9a5416d1b741
David Ghazi <davidaghazi@gmail.com>
2021-05-05 23:01:56 -0400

    completed slideshow with images + created 2 html pages as routes for the dropdown menu in the navbar

---
+++ script.js
@@ -0,0 +1,18 @@
+// Code written here to create a slideshow for the background images
+// Used jQuery here for simplicity in order to create the "slideshow" for the images
+(function ($, interval, slides) {
+  var i = 0;
+  var handle = setInterval(function () {
+    $("body").css("background-image", "url('" + slides[i] + "')");
+
+    i++;
+
+    if (i >= slides.length) {
+      i = 0;
+    }
+  }, interval);
+})(jQuery, 2000, [
+  "./images/banner1.png",
+  "./images/banner2.png",
+  "./images/banner3.jpg",
+]);
